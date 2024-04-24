d23e5f96575c2fd8855fb33f388a98f1da3b730e
dompitula <pituladom@gmail.com>
2024-04-24 15:13:27 +0200

    setup for build

---
+++ setup.py
@@ -0,0 +1,16 @@
+from cx_Freeze import setup, Executable
+
+# Define the executable information
+target = Executable(
+    script="FreeSpace.py",
+    base="Win32GUI",
+    icon="FreeSpace.ico"
+    )
+
+# Create the setup object
+setup(
+    name="FreeSpace Manager",
+    version="2.0",
+    description="This program helps you monitor your disk space and provides options to restart your PC or delete temporary files.",
+    executables = [target]
+)
